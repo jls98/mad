040ef3615ef87786a9489aa631c70f47c975ac2e
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-16 18:02:56 +0100

    fnand

--- assignment3/test_tasks.c
+++ assignment3/test_tasks.c
@@ -217,9 +217,7 @@ void test_fNAND(){
 	flush(c);
 	
 	fNAND(c, a, b);
-	fence();
 	time = probe(c);
-	fence();
 	CU_ASSERT_TRUE(time<THRESHOLD);
 	//if(time>=THRESHOLD) 
 	printf("\n\nfNAND case notA, notB: c %lu\n", time);
