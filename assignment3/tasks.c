2bf6e0392346cf0a0b2f80de6173db83d58e0ac5
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-16 09:37:59 +0100

    fnand

--- assignment3/tasks.c
+++ assignment3/tasks.c
@@ -63,7 +63,7 @@ static void fNOT(void *out, void *in){
 		// BEGIN Spec part 		
 		"xor rax, rax;"
 		// BEGIN delay ops
-		".rept 20;"
+		".rept 100;"
 		"mov rax, [rsp+rax];"
 		"and rax, 0x0;"
 		".endr;"
