ebe6ae5a353bfa68da567ff8271bc17dedeefe23
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-01-17 12:57:54 +0100

    w9

---
+++ workshop9/spectre.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// init covert channel
+void cc_init();
+
+// set channel for sending byte
+void cc_setup();
+
+// transmit a value through channel
+void cc_transmit(uint8_t value);
+
+// return received value or -1 if error
+uint8_t cc_receive();
+
+
+
+void *cc_init(){
+	
+    void *cc = malloc(256*sizeof(void *)); // allocate space for 256 cache lines
+    return cc;
+
+
+}
+
+void cc_setup(){
+    void *cc = cc_init();
+    for(int i=0;i<256;i++){
+        flush(&cc[0]);
+        printf("%i\n",i);
+    }
+}
+
+
+void flush(void *p)
+{
+    __asm__ volatile ("clflush [%0]":: "r" (p));
+}
+
+
+
+
+
+
+
