cdfa0b4da37f6ea764f4851dbc34fa5c94639719
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-18 03:12:48 +0100

    xor

--- assignment3/test_tasks.c
+++ assignment3/test_tasks.c
@@ -948,7 +948,7 @@ int main() {
    // CU_add_test(suite, "Test fNOT", test_fNOT);
     //CU_add_test(suite, "Test fNOTN", test_fNOTN);
     //CU_add_test(suite, "Test fNANDN", test_fNANDN);
-    CU_add_test(suite, "Test fNORN", test_fNORN);
+   // CU_add_test(suite, "Test fNORN", test_fNORN);
     CU_add_test(suite, "Test fXOR", test_fXOR);
    // CU_add_test(suite, "Test fNAND", test_fNAND); 
     //CU_add_test(suite, "Test fAND", test_fAND); 
