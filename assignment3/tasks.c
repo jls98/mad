2f15e646166f338976cd5b9c9a1a2a2c6540f963
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-18 14:45:54 +0100

    check test

--- assignment3/tasks.c
+++ assignment3/tasks.c
@@ -356,7 +356,6 @@ static void fXOR(void *out, void *in1, void *in2, void **buf){
 	fNOTN(in1, buf[0], buf[1]); // 6 dump
 	fNOTN(in2, buf[2], buf[3]); // 
 
-	printf("after nota %lu %lu\n", probe(buf[0]), probe(buf[1]));
 	fence();
 	//!(!A & !B)
 	fNAND(buf[4], buf[0], buf[2]);
