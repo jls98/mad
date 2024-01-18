c78b6acf0fbcdf3308a26e6b33e1cb8a4b86fb87
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-18 01:14:48 +0100

    added a

--- assignment3/tasks.c
+++ assignment3/tasks.c
@@ -130,7 +130,7 @@ static void fNOTN(void* out_1, void* out_2, void* out_3, void* out_4, void* in){
 		"call fNOTN_1;"
 		"xor rax, rax;"
 		// BEGIN delay ops 
-		".rept 10;" // deplen
+		".rept 1;" // deplen
 		"mov rax, [rsp+rax];"
 		"and rax, 0x0;"
 		".endr;"
