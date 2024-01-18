f356bbbd426403245c07644daa79ce6162eeb855
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-18 03:27:15 +0100

    check test

--- assignment3/test_tasks.c
+++ assignment3/test_tasks.c
@@ -947,8 +947,8 @@ int main() {
     CU_pSuite suite = CU_add_suite("Test Suite assignment 3", NULL, NULL);
     CU_add_test(suite, "Test fNOT", test_fNOT);
     CU_add_test(suite, "Test fNOTN", test_fNOTN);
-    CU_add_test(suite, "Test fNANDN", test_fNANDN);
-    //CU_add_test(suite, "Test fNORN", test_fNORN);
+    //CU_add_test(suite, "Test fNANDN", test_fNANDN); // TODO
+    CU_add_test(suite, "Test fNORN", test_fNORN);
     //CU_add_test(suite, "Test fXOR", test_fXOR);
     CU_add_test(suite, "Test fNAND", test_fNAND); 
     CU_add_test(suite, "Test fAND", test_fAND); 
