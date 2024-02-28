71c5929ad784cc96ad3ed31cecc4447806214d86
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-28 01:13:22 +0100

    added testcase cc init

--- assignment4/test_meltdown-template.c
+++ assignment4/test_meltdown-template.c
@@ -2,13 +2,18 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
+void test_cc_init(){
+    cc_init();
+    CU_ASSERT_TRUE(cc_buffer != NULL);
+}
 
 
 int main() {
     CU_initialize_registry();
 
     CU_pSuite suite = CU_add_suite("Test Suite assignment 4", NULL, NULL);
-   
+   	CU_add_test(suite, "test_cc_init", test_cc_init);
+
 
 	CU_basic_run_tests();
     CU_cleanup_registry();
