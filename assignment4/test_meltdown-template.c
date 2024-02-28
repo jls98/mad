079165ac1e31020dbda3c3f0ed199feb2202bf15
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-28 01:59:20 +0100

    added testcase cc setup

--- assignment4/test_meltdown-template.c
+++ assignment4/test_meltdown-template.c
@@ -20,7 +20,7 @@ void test_cc_setup(){
         time = my_rdtsc();
         maccess(cur_adrs);
         time = my_rdtsc() - time;
-        printf("%c %lx\n", *((char *)cur_adrs), (char *)cur_adrs);
+        printf("%x %lx\n", *((char *)cur_adrs), (char *)cur_adrs);
         CU_ASSERT_TRUE(*((char *)cur_adrs) == 0xaa);
         CU_ASSERT_TRUE(time > threshold);
         flush(cur_adrs);
