0bc9313abbed4a96ee752f0a68f0087ed20ea21f
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-28 01:05:18 +0100

    added testsuite

---
+++ assignment4/test_meltdown-template.c
@@ -0,0 +1,17 @@
+#include "tasks.c"
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+
+
+int main() {
+    CU_initialize_registry();
+
+    CU_pSuite suite = CU_add_suite("Test Suite assignment 4", NULL, NULL);
+   
+
+	CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return 0;
+}
