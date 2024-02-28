aa89fab0be6dcb25a6dd7b2cfde4eab017214acd
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-28 20:02:49 +0100

    uts dfespair

--- assignment4/meltdown-template.c
+++ assignment4/meltdown-template.c
@@ -174,7 +174,7 @@ static void attack_init_uts_ns(){
     int leaked_val[100];
     for (int i=0;i<100;i++) leaked_val[i] = -1;
     
-    for(int j=0;j<10000;j++){}
+    for(int j=0;j<10000;j++){
         for (int i=0;i<100;i++){
             if (leaked_val[i] == -1){
                 leaked_val[i]=do_meltdown(target_adrs+2*i);
